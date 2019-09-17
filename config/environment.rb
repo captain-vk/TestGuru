a0cb6b3fd533cc83d98ec821237639150934e156
captain-vk <vk2609@rambler.ru>
2019-09-17 18:12:19 +0300

    fix part2

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 # Load the Rails application.
 require_relative 'application'
 
-VERSION = '1.0.0'
 # Initialize the Rails application.
 Rails.application.initialize!
-
