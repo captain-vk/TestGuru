49f1d3ea041c5b8b720a5a6615355ecdcc45e69f
CaptainK <vk2609@rambler.ru>
2019-09-22 22:51:47 +0300

    part3_unfinished

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+	def self.list_of_test(name)
+      joins(Category)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create!([{title: 'Frontend'},
+							   {title: 'Backend'},
+							   {title: 'Mobile'}])
+
+tests = Test.create!([{title: 'Ruby', level: 1, category_id: categories[1]},
+					  {title: 'JavaScript', level: 5, category_id: categories[0]},
+					  {title: 'Java', level: 1, category_id: categories[2]}])
+
+users = User.create([{login: 'Ivan', password: 'qwerty'}])
+
+questions = Question.create!([{body: 'Кто создал язык Ruby?', test_id: tests[0]},
+						  {body: 'Что такое замыкания?', test_id: tests[1]},
+						  {body: 'Переведи на англ. Сборщик мусора', test_id: tests[2]}])
+
+answers = Answer.create!([{body: 'Юкихиро Мацумото', correct: true, question_id: questions[0]},
+						  {body: 'Когда проводка замыкает', correct: false, question_id: questions[1]},
+						  {body: 'Garbage collector', correct: true, question_id: questions[2]}])
