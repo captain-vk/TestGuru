46c3b9cd4f6ee5cd11be1dd5fc1e82a4a9e0e372
captain-vk <vk2609@rambler.ru>
2019-09-23 19:15:56 +0300

    fix part3

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
-	def self.list_of_test(name)
-      joins(Category)
+  def self.by_test(category_name)
+    joins('INNER JOIN categories ON tests.category_id = categories.id')
+        .where(categories: { title: category_name })
   end
 end
