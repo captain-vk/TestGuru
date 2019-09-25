8836bef3157289078f1c80f6f92558f2483b2a23
captain-vk <vk2609@rambler.ru>
2019-09-25 11:57:39 +0300

    fix part3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
    def search_test(level)
    	Test.joins('INNER JOIN results ON results.test_id = tests.id')
-   	.where(results: { user_id: '0' }, tests: { level: level })
+   	.where(results: { user_id: id }, tests: { level: level })
    end  
 end
