8db99ec59e683a77496346d0d9d5aea50b926205
CaptainK <vk2609@rambler.ru>
2019-09-29 17:02:23 +0300

    part5_associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :categories
+	has_many :questions
+  
+  has_many :users, through: :results
+  has_many :results
   def self.by_test(category_name)
     joins('INNER JOIN categories ON tests.category_id = categories.id')
         .where(categories: { title: category_name }).order(id: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-   def search_test(level)
-   	Test.joins('INNER JOIN results ON results.test_id = tests.id')
-   	.where(results: { user_id: id }, tests: { level: level })
-   end  
+	has_many :tests, through: :results
+  has_many :results
+
+  def search_test(level)
+    Test.joins('INNER JOIN results ON results.test_id = tests.id')
+    .where(results: { user_id: id }, tests: { level: level })
+  end  
 end
