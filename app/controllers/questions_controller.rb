a6d6fbe60ce143e77642e8b83432451c013d2875
captain-vk <vk2609@rambler.ru>
2019-10-09 17:28:38 +0300

    part7

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,27 +1,42 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
+  before_action :find_test
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
-    # render plain: 'All questions'
-    @test = Test.find(params[:test_id])
     render json: @test.questions
-  end   
+  end
 
   def show
     @question = Question.find(params[:id])
     render plain: @question.body
   end
 
-  def new
+  def new; end
+
+  def create
+    @question = @test.questions.build(question_params)
+    render plain: 'Вопрос создан' if @question.save
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    render plain: 'Вопрос удален'
+  end
+
+  private
+
+  def find_test
     @test = Test.find(params[:test_id])
   end
 
-  def create
-    question = Question.create(question_params)
-    render plain: question.inspect
-  end
-
-private
-
   def question_params
     params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Не найдено!'
   end
 end
